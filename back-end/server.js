8dac549fc227f3a244061d3824c9fe416f3e03f7
Idowu Joseph <idowujo672@gmail.com>
2025-04-11 11:44:24 +0100

    fix: correct stripe.png import path for vercel deployment

--- back-end/server.js
+++ back-end/server.js
@@ -14,15 +14,7 @@ connectDB()
 connectCloudinary()
 app.use(express.json())
 app.use(express.urlencoded({ extended: true })); 
-app.use(cors({
-  origin: [
-    "http://localhost:5173",  
-      "http://localhost:5174",       // Local development
-    "https://adminparnel.vercel.app",  // Admin panel
-    "https://rattleecommercestore.vercel.app/login" // Main frontend
-],
-    credentials: true                 
-}));
+app.use(cors())
 app.use('/api/user', userRouter)
 app.use('/api/product', productRouter)
 app.use('/api/cart', cartRouter)
